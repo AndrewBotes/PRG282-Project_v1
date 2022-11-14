66fd9243e2238b8d459786b070395a785cc3dc5f
Mathys Johannes van Heerden <576482@student.belgiumcampus.ac.za>
2022-11-14 21:04:29 +0200

    test 1

--- Program.cs
+++ Program.cs
@@ -17,6 +17,7 @@ namespace PRG282_Project_v1
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            Console.WriteLine("Hello");
         }
     }
 }
